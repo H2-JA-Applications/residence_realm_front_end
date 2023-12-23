710696e720a0b59eb3826837aa66a1d01597baa9
Henrymelendez <henryomarm@gmail.com>
2023-12-23 13:24:36 -0500

    Added way to handle token expiration

--- src/utils/useToken.js
+++ src/utils/useToken.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 export const axiosInterceptor = () => {
   axios.interceptors.request.use(config => {
@@ -6,4 +7,15 @@ export const axiosInterceptor = () => {
     config.headers.Authorization = token ? `Bearer ${token}` : '';
     return config;
   });
+
+  axios.interceptors.response.use(
+    response => response,
+    error => {
+      if (error.response && error.response.status === 401) {
+        localStorage.clear();
+        useHistory().push('/');
+      }
+      return Promise.reject(error);
+    }
+  );
 }
