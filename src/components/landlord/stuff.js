ada4fae4c4e89ec8952dfa9f497708fa00298c65
Henrymelendez <henryomarm@gmail.com>
2023-12-26 12:28:13 -0500

    changes to use axios

--- src/components/landlord/stuff.js
+++ src/components/landlord/stuff.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export const useLandlordProperties = () => {
   const [landlordProperties, setLandlordProperties] = useState([]);
 
   useEffect(() => {
-      fetch('http://localhost:8080/api/properties/landlord/properties')
-          .then(response => response.json())
-          .then(data => setLandlordProperties(data))
-          .catch(error => console.error('Error:', error));
+    axios.get('http://localhost:8080/api/properties/landlord/properties')
+      .then(response => {
+        setLandlordProperties(response.data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
   }, []);
 
   return landlordProperties;
