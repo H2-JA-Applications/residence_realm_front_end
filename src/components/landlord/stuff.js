39b06d08d0817690cbf8061b74e045b52c90a590
Henrymelendez <henryomarm@gmail.com>
2023-12-26 11:23:26 -0500

    pulling properties

--- src/components/landlord/stuff.js
+++ src/components/landlord/stuff.js
@@ -1,27 +1,2 @@
-export const landlord_properties = [
-  {
-    payment_ID: '1',
-    rent: '$2000',
-    dueDate: '12/14/2023',
-  },
-  {
-    payment_ID: '1',
-    rent: '$2000',
-    dueDate: '12/14/2023',
-  },
-  {
-    payment_ID: '1',
-    rent: '$2000',
-    dueDate: '12/14/2023',
-  },
-  {
-    payment_ID: '1',
-    rent: '$2000',
-    dueDate: '12/14/2023',
-  },
-  {
-    payment_ID: '1',
-    rent: '$2000',
-    dueDate: '12/14/2023',
-  }
-];
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
